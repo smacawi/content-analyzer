948f7c4123e37f07071482e528d411203166e5f7
amr-amr <andrei.romascanu@mail.mcgill.ca>
2021-04-26 13:31:47 +0100

    rnn classifier w/ metrics

--- content_analyzer/models/rnn_classifier.py
+++ content_analyzer/models/rnn_classifier.py
@@ -3,13 +3,14 @@ from typing import Dict
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from collections import defaultdict
 
 from allennlp.data import Vocabulary
 from allennlp.models import Model
 from allennlp.modules import Seq2VecEncoder, TextFieldEmbedder
 from allennlp.nn import InitializerApplicator
 from allennlp.nn.util import get_text_field_mask
-from allennlp.training.metrics import CategoricalAccuracy
+from allennlp.training.metrics import CategoricalAccuracy, F1Measure
 
 
 @Model.register('rnn_classifier')
@@ -30,9 +31,11 @@ class RnnClassifier(Model):
         self._dropout = nn.Dropout(dropout)
         self._num_labels = vocab.get_vocab_size(namespace=label_namespace)
         self._classification_layer = nn.Linear(self._classifier_input_dim, self._num_labels)
-        self._accuracy = CategoricalAccuracy()
         self._loss = nn.CrossEntropyLoss()
-
+        self.itol = vocab.get_index_to_token_vocabulary(namespace=label_namespace) # idx to label
+        self._prf_metrics = {l: F1Measure(i) for i,l in self.itol.items()}
+        self._accuracy = CategoricalAccuracy()
+        self._label_counts = {l: 0 for i,l in self.itol.items()}
         self.oov_id = vocab.get_token_index(vocab._oov_token)
         self.oov = {}
 
@@ -55,22 +58,30 @@ class RnnClassifier(Model):
 
         oov = {k: int((t==self.oov_id).sum()) for k,t in tokens.items()}
         for k,v in oov.items():
-            if k in self.oov:
-                self.oov[k] += v
-            else:
-                self.oov[k] = v
+            if k not in self.oov:
+                self.oov[k] = 0
+            self.oov[k] += v
 
         if label is not None:
             loss = self._loss(logits, label.long().view(-1))
+            for metric in self._prf_metrics.values():
+                metric(logits, label.squeeze(-1))
+            for i,l in self.itol.items():
+                self._label_counts[l] += (label==i).sum()
+            self._accuracy(logits, label.squeeze(-1))
             output_dict['loss'] = loss
-            self._accuracy(logits, label)
-
         return output_dict
 
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
-        oov_metrics = {f"oov_{k}": v for k,v in self.oov.items()}
-        metrics = {'accuracy': self._accuracy.get_metric(reset)}
-        metrics.update(oov_metrics)
+        metrics = {f"oov_{k}": v for k,v in self.oov.items()}
+        # precision/recall/f1
+        metrics.update({f"{metric_name}_P": metric.get_metric()[0] for metric_name, metric in self._prf_metrics.items()})
+        metrics.update({f"{metric_name}_R": metric.get_metric()[1] for metric_name, metric in self._prf_metrics.items()})
+        metrics.update({f"{metric_name}_F1": metric.get_metric(reset)[2] for metric_name, metric in self._prf_metrics.items()})
+        metrics.update({"accuracy": self._accuracy.get_metric(reset)})
+        metrics.update({f"{label_name}_count": int(c) for label_name, c in self._label_counts.items()})
         if reset:
             self.oov = {}
+            self._label_counts = {l: 0 for i, l in self.itol.items()}
+
         return metrics
